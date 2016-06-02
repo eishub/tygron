962229bb4e06af168d05042cf443e3f893570358
hmyeh <h.m.yeh@student.tudelft.nl>
2016-06-02 11:04:02 +0200

    Implemented J2PopupData with more useful information

--- environment/src/main/java/tygronenv/translators/J2PopupData.java
+++ environment/src/main/java/tygronenv/translators/J2PopupData.java
@@ -1,5 +1,7 @@
 package tygronenv.translators;
 
+import java.util.List;
+
 import eis.eis2java.exception.TranslationException;
 import eis.eis2java.translation.Java2Parameter;
 import eis.eis2java.translation.Translator;
@@ -7,8 +9,12 @@ import eis.iilang.Function;
 import eis.iilang.Identifier;
 import eis.iilang.Numeral;
 import eis.iilang.Parameter;
+import eis.iilang.ParameterList;
+import nl.tytech.core.client.event.EventManager;
+import nl.tytech.core.net.serializable.MapLink;
 import nl.tytech.data.engine.item.Building;
 import nl.tytech.data.engine.item.PopupData;
+import nl.tytech.data.engine.item.SpecialOption;
 
 /**
  * Translate {@link Building} into building(ID, name, [categories], timestate).
@@ -22,9 +28,18 @@ public class J2PopupData implements Java2Parameter<PopupData> {
 
 	@Override
 	public Parameter[] translate(PopupData b) throws TranslationException {
-
-		return new Parameter[] { new Function("request", new Identifier(b.getType().toString()), new Numeral(b.getID()),
-				translator.translate2Parameter(b.getAnswers())[0]) };
+	    String typeOfPopup = null;
+	    if (b.getContentMapLink() == MapLink.SPECIAL_OPTIONS) {
+	        SpecialOption specialOption = EventManager.getItem(MapLink.SPECIAL_OPTIONS, b.getContentLinkID());
+	        typeOfPopup = specialOption.getType().name();
+	    } else if (b.getContentMapLink() == MapLink.BUILDINGS) {
+	        typeOfPopup = "PERMIT";
+	    } else {
+	        typeOfPopup = "POPUP";
+	    }
+		return new Parameter[] { new Function("request", new Identifier(b.getType().name()), new Identifier(typeOfPopup), new Numeral(b.getID()),
+		        new Numeral(b.getContentLinkID()), translator.translate2Parameter(b.getMultiPolygon())[0],
+				getVisibleForStakeholderIDs(b.getVisibleForStakeholderIDs()), translator.translate2Parameter(b.getAnswers())[0]) };
 	}
 
 	@Override
@@ -32,4 +47,12 @@ public class J2PopupData implements Java2Parameter<PopupData> {
 		return PopupData.class;
 	}
 
+	public ParameterList getVisibleForStakeholderIDs(List<Integer> visibleList) {
+	    ParameterList parVisibleList = new ParameterList();
+	    for (Integer id : visibleList) {
+	        parVisibleList.add(new Numeral(id));
+	    }
+	    return parVisibleList;
+	}
+	
 }
