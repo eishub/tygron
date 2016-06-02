1c05fc40719a76df417aaebce37f035514681376
hmyeh <h.m.yeh@student.tudelft.nl>
2016-06-02 12:24:28 +0200

    Fixed some checkstyle, pmd and FindBugs

--- environment/src/main/java/tygronenv/translators/J2PopupData.java
+++ environment/src/main/java/tygronenv/translators/J2PopupData.java
@@ -12,47 +12,69 @@ import eis.iilang.Parameter;
 import eis.iilang.ParameterList;
 import nl.tytech.core.client.event.EventManager;
 import nl.tytech.core.net.serializable.MapLink;
-import nl.tytech.data.engine.item.Building;
 import nl.tytech.data.engine.item.PopupData;
 import nl.tytech.data.engine.item.SpecialOption;
+import nl.tytech.data.engine.item.SpecialOption.Type;
 
 /**
- * Translate {@link Building} into building(ID, name, [categories], timestate).
- * 
+ * Translate {@link Popup} into request(type, category, contentlinkID,
+ * multipolygon, [visibleStakeholderIDs], [answers]).
  * @author W.Pasman
  *
  */
 public class J2PopupData implements Java2Parameter<PopupData> {
 
+    /**
+     * Translator for additional translations.
+     */
 	private final Translator translator = Translator.getInstance();
 
+	/**
+	 * Empty constructor.
+	 */
+	public J2PopupData() { }
+
+	/**
+	 * Method to translate the PopupData class.
+	 */
 	@Override
-	public Parameter[] translate(PopupData b) throws TranslationException {
+	public Parameter[] translate(final PopupData popup) throws TranslationException {
 	    String typeOfPopup = null;
-	    if (b.getContentMapLink() == MapLink.SPECIAL_OPTIONS) {
-	        SpecialOption specialOption = EventManager.getItem(MapLink.SPECIAL_OPTIONS, b.getContentLinkID());
-	        typeOfPopup = specialOption.getType().name();
-	    } else if (b.getContentMapLink() == MapLink.BUILDINGS) {
+	    if (popup.getContentMapLink() == MapLink.SPECIAL_OPTIONS) {
+	        SpecialOption specialOption = EventManager.getItem(MapLink.SPECIAL_OPTIONS, popup.getContentLinkID());
+	        Type optionType = specialOption.getType();
+	        typeOfPopup = optionType.name();
+	    } else if (popup.getContentMapLink() == MapLink.BUILDINGS) {
 	        typeOfPopup = "PERMIT";
 	    } else {
 	        typeOfPopup = "POPUP";
 	    }
-		return new Parameter[] { new Function("request", new Identifier(b.getType().name()), new Identifier(typeOfPopup), new Numeral(b.getID()),
-		        new Numeral(b.getContentLinkID()), translator.translate2Parameter(b.getMultiPolygon())[0],
-				getVisibleForStakeholderIDs(b.getVisibleForStakeholderIDs()), translator.translate2Parameter(b.getAnswers())[0]) };
+		return new Parameter[] {new Function("request", new Identifier(popup.getType().name()),
+		        new Identifier(typeOfPopup), new Numeral(popup.getID()),
+		        new Numeral(popup.getContentLinkID()),
+		        translator.translate2Parameter(popup.getMultiPolygon())[0],
+				getVisibleForStakeholderIDs(popup.getVisibleForStakeholderIDs()),
+				translator.translate2Parameter(popup.getAnswers())[0]) };
 	}
 
+	/**
+	 * Method to return the PopupData class.
+	 */
 	@Override
 	public Class<? extends PopupData> translatesFrom() {
 		return PopupData.class;
 	}
 
-	public ParameterList getVisibleForStakeholderIDs(List<Integer> visibleList) {
+	/**
+	 * Method to change the visibleStakeholderID list into a ParameterList.
+	 * @param visibleList list of stakeholderIDs.
+	 * @return ParameterList of shIDs.
+	 */
+	public ParameterList getVisibleForStakeholderIDs(final List<Integer> visibleList) {
 	    ParameterList parVisibleList = new ParameterList();
 	    for (Integer id : visibleList) {
 	        parVisibleList.add(new Numeral(id));
 	    }
 	    return parVisibleList;
 	}
-	
 }
